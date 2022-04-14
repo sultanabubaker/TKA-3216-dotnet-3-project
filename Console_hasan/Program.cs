75f67635cec15881499b9e0a539182ce0161301a
SultanAbuBaker <m8coollg@gmail.com>
2022-04-14 18:02:20 +0300

    first

---
+++ Console_hasan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_hasan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
